8e1f01a7cf9c43df596eee85eb5fe02b84801d34
SalihKARAHAN <salihkarahan@outlook.com>
2016-04-21 22:43:40 +0300

    fixed error.

--- decleration/nodejs.d.ts
+++ decleration/nodejs.d.ts
@@ -1,3 +1,8 @@
-declare module 'http'{
-    export function listen(url:string, port:number):void;
+declare module 'http' {
+    export interface Server {
+        listen(port: number, url: string): void;
+        listen(port: number, callback: any): void;
+        listen(port: number, url: string, callback: any): void;
+    }
+    export function createServer(httpManager: any): Server;
 }
--- src/Application.ts
+++ src/Application.ts
@@ -1,7 +1,15 @@
 import http = require('http');
 
-export class Application {
-    Start(url, port): void {
-        http.listen(url,port);
+class Application {
+    Start(port:number, callback:any): void {
+        var server = http.createServer(function (request, response) {
+            console.log(request.url);
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.write('<b>Hello NodeTS!</b>');
+            response.end();
+        })
+        server.listen(port,callback);
     }
 }
+
+export = Application;
