002c4b628c5d5dd5bf082fe75edcb43f2f1fdd28
Salih KARAHAN <salihkarahan@outlook.com>
2016-06-24 23:34:33 +0300

    deleted custom definition file for nodejs
    deleted custom definition file for nodejs

--- decleration/nodejs.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'http' {
-    export interface Server {
-        listen(port: number, url: string): void;
-        listen(port: number, callback: any): void;
-        listen(port: number, url: string, callback: any): void;
-    }
-    export function createServer(httpManager: any): Server;
-}
